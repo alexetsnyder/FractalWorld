ae8b40f5b2e559f4304132951ee0772b69918074
Alex E.T. Snyder <alexetsnyder@gmail.com>
2024-10-20 10:27:13 -0400

    Update Main.cpp
    Added stubs for each of the different options in the main menu, and added those functions to an unordered_map which matches entered command with a functions pointer to the new stubs.

--- FractalWorld/Main.cpp
+++ FractalWorld/Main.cpp
@@ -3,13 +3,31 @@
 #include <iostream>
 #include <limits>
 #include <string>
+#include <vector>
+#include <unordered_map>
 
 #define NOMINMAX
 #include <Windows.h>
 
+bool clear_screen_flag{ true };
+
 void main_menu();
 void print_main_menu();
+void new_game();
+void load_game();
+void settings();
+void credits();
+void print_back_prompt();
 int clear_screen();
+
+std::unordered_map<std::string, void(*)()> commands
+{
+	{ "exit", []() { std::cout << "Thank you for playing!\n"; } },
+	{ "new", &new_game },
+	{ "load", &load_game },
+	{ "settings", &settings },
+	{ "credits", &credits },
+};
 
 int main(int argc, char** argv)
 {
@@ -22,7 +40,10 @@ void main_menu()
 {
 	while (true)
 	{
-		clear_screen();
+		if (clear_screen_flag)
+		{
+			clear_screen();
+		}
 		print_main_menu();
 
 		std::string input{};
@@ -34,9 +55,14 @@ void main_menu()
 		}
 
 		std::transform(input.begin(), input.end(), input.begin(), [](unsigned char c) { return std::tolower(c); });
+
+		if (commands.count(input) == 1)
+		{
+			commands.at(input)();
+		}
+
 		if (input == "exit")
 		{
-			std::cout << "Thank you for playing!\n";
 			break;
 		}
 	}
@@ -50,6 +76,42 @@ void print_main_menu()
 	std::cout << "3. Settings\n";
 	std::cout << "4. Credits\n";
 	std::cout << "5. Exit\n";
+}
+
+void new_game()
+{
+	std::cout << "You are on a path in the woods. You are walking towards an unknown destination.\n"; 
+	std::cout << "The hoary faces of ancient trees watch your progress: silent judges of what's to come.\n";
+	print_back_prompt();
+}
+
+void load_game()
+{
+	std::cout << "Saves:\n";
+	std::cout << "1. Brave New World.\n";
+	std::cout << "2. Fake Save 2.\n";
+	std::cout << "3. Bloodroot.\n";
+	print_back_prompt();
+}
+
+void settings()
+{
+	std::cout << "Settings:\n";
+	std::cout << "Clear Screen: " << std::boolalpha << clear_screen_flag << "\n";
+	print_back_prompt();
+}
+
+void credits()
+{
+	std::cout << "Credits:\n";
+	std::cout << "Autumn Snyder\n";
+	print_back_prompt();
+}
+
+void print_back_prompt()
+{
+	std::cout << "Press enter to return: ";
+	std::cin.ignore();
 }
 
 //From: https://learn.microsoft.com/en-us/windows/console/clearing-the-screen
